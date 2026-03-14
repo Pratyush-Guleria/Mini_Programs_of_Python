76cbeaf1e716e980d7136c18eb96f80500f874f6
Pratyush-Guleria <258414203+Pratyush-Guleria@users.noreply.github.com>
2026-03-14 15:50:05 +0530

    Added a feature

--- Smart_ATM_Counter.py
+++ Smart_ATM_Counter.py
@@ -1,18 +1,38 @@
 correct_pin = 123
 attempt = 0 
-
+balance = 500
 while True:
 
-    user_input = input("Enter pin :")
+    user_input = input("Enter pin :").strip()
 
     if user_input =="":
         print("Sorry you didn't entered anything")
         continue 
+    
+    if not user_input.isdigit():
+        print("Please enter only number (0-9)")
+        continue
 
     pin = int(user_input)
     if pin == correct_pin:
         print ("Correct pin")
-        break
+
+        check = input("Do you want to check your balance (Y/N) :").upper()
+
+        if check =="Y":
+            print(f"Your balance is :${balance}")
+
+            break
+
+        elif check =="N":
+            print("Ok thanks")
+
+            break
+
+        else:
+            print("Sorry invalid option, exiting")
+
+            break
 
     else:
         attempt +=1
