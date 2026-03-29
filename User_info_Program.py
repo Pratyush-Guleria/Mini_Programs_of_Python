728db3836369e073d293db533371712f725cb0c7
Pratyush-Guleria <258414203+Pratyush-Guleria@users.noreply.github.com>
2026-03-29 17:37:12 +0530

    Added a feature

--- User_info_Program.py
+++ User_info_Program.py
@@ -1,30 +1,67 @@
+"""
+User Identification and Age Classification System
+Purpose: To collect user details, assign titles based on gender, 
+         and categorize them by age group with error handling. 
+"""
+
+
+#--- Step 1 : User Name Input & Validation ---
+
 while True :
 
-    name = input("What's your name :").strip().capitalize()
+    name = input("What's your name :").strip().title()
 
-    if name =="":
+    if not name:
         print("You didn't enter your name")
+        continue
+
     else:
-        print(f"Hey : {name}")
+        print(f"Hello : {name}")
         break
 
+#---Step 2 : User Gender input and Title assignment ---
+
+while True:
+        
+        gender_input = input("what's your gender (M/F) :").strip().capitalize()
+
+        
+        if gender_input =="M":
+            gender = "Mr."
+            break
+
+        elif gender_input =="F":
+            gender = "Ms."
+            break
+        
+        else:
+            print("Plz enter M or F only")
+            continue
+
+# --- Step 3 : User Age input and Classification
+
 while True :
 
-    age_input = input(f"Hey : {name}, What's your age :").strip()
+    age_input = input("What's your age :").strip()
 
     try:
         age = int(age_input)
 
         if age < 0:
-            print(f"Sorry :{name}, You are not born yet")
+            print(f"Sorry : {gender} {name}, You are not born yet")
+            continue
 
         elif age < 18:
-            print(f"Hey :{name}, Your are a minor")
+            print(f"Category: Minor | User: {gender} {name}")
+            break
+ 
+        elif age >=60:
+            print(f"Category: Senior Citizen | User: {gender} {name}")
             break
 
 
         else:
-            print(f"Hey :{name}, You are an adult ")
+            print(f"Category: Adult | User: {gender} {name}")
             break
 
     except ValueError:
