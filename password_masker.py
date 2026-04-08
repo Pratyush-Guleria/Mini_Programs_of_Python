47ec0d2d490242a558d303c922c756e9ea3a141b
Pratyush-Guleria <258414203+Pratyush-Guleria@users.noreply.github.com>
2026-04-08 17:42:35 +0530

    Added length validation to password masker

--- password_masker.py
+++ password_masker.py
@@ -1,5 +1,10 @@
 password = input("What's your password :").strip()
 
-result = password[0] + "*" *(len(password) - 2) + password[-1]
+password_len = len(password)
+print(f"Lenght of your password :{password_len}")
+if password_len >= 3:
+    result = password[0] + "*" *(len(password) - 2) + password[-1]
+    print(result)
 
-print(result)
+else:
+    print("Your password is too short")
