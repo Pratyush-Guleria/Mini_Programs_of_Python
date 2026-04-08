68c0fe34a8680b90d7ea6437a828526c64982913
Pratyush-Guleria <258414203+Pratyush-Guleria@users.noreply.github.com>
2026-04-08 17:25:01 +0530

    Added Professional status feature

--- id_card_generator.py
+++ id_card_generator.py
@@ -8,9 +8,16 @@ DOB = int(input("In which year you born :").strip())
 
 age = 2026 - DOB
 
+if age >= 18:
+     Professional_status = "You are eligible for Internships"
+
+else :
+     Professional_status = "Junior Student"
+
 print("-------------------------")
 print(f"Student ID: {name}")
 print(f"AGE: {age}")
 print(f"COLLEGE: {college}")
 print(f"SKILL: {skill}")
+print(f"Professional Status: {Professional_status}")
 print("-------------------------")
